8521b59009db58c9aa00adf09247632e04267262
polgonzalez05 <polgonzalezalcazar@gmail.com>
2026-05-19 10:04:12 +0200

    Player Acabado

--- App/Source/Game/Player.cs
+++ App/Source/Game/Player.cs
@@ -1,34 +1,76 @@
 ﻿using SFML.Window;
 using SFML.Graphics;
 using SFML.System;
-using System.Collections.Generic;
 using System;
 
 namespace TcGame
 {
   public class Player : StaticActor
   {
+    private float coolDown = 0.5f;
+    private float currentCoolDown = 0.0f;
+
+    public int HitPoints = 100;
+
     public Player()
     {
       Layer = ELayer.Middle;
       Sprite = newRandomSprite();
       Center();
       Position = new Vector2f(780.0f, 350.0f);
-    } 
-    
-  public override void Update(float dt)
+      
+      ChangeCoolDown(0.2f);
+
+    }
+
+    public override void Update(float dt)
     {
       Follow();
+
+      currentCoolDown -= dt;
+      if (Mouse.IsButtonPressed(Mouse.Button.Left) && currentCoolDown <= 0.0f)
+      {
+        Shoot();
+        currentCoolDown = coolDown;
+      }
+
       base.Update(dt);
     }
+
     private void Follow()
     {
       Speed = 0;
       Forward = (Engine.Get.MousePos - Position).Normal();
-      Rotation = (float) Math.Atan2(Forward.Y, Forward.X) * MathUtil.RAD2DEG + 90;
+      Rotation = (float)Math.Atan2(Forward.Y, Forward.X) * MathUtil.RAD2DEG;
     }
 
-    private Sprite newRandomSprite()
+
+    public void ChangeCoolDown(float newCoolDown)
+    {
+      coolDown = newCoolDown;
+    }
+
+    public void Shoot()
+    {
+      Bullet bullet = Engine.Get.Scene.Create<Bullet>();
+
+      bullet.Position = Position + Forward * 40.0f;
+      bullet.Forward = Forward;
+      bullet.DamagePlayer = false;
+      bullet.Layer = ELayer.Middle;
+      bullet.Center();
+    }
+    public void TakeDamage(int damage)
+    {
+      HitPoints -= damage;
+
+      if (HitPoints <= 0)
+      {
+        Destroy();
+      }
+    }
+
+  private Sprite newRandomSprite()
     {
       Random rand = new Random();
       int random = rand.Next(0, 2);
